7dfcfcf241b3950ecfd8d92ad60f5ac779894bf5
TemaFlux <temaflex1337@gmail.com>
2025-01-30 10:21:28 +0200

    Move to usage XSound from XSeries

--- src/main/java/com/extendedclip/deluxemenus/utils/SoundUtils.java
+++ src/main/java/com/extendedclip/deluxemenus/utils/SoundUtils.java
@@ -1,13 +1,13 @@
 package com.extendedclip.deluxemenus.utils;
 
+import com.cryptomorin.xseries.XSound;
 import org.bukkit.Sound;
-
 import java.lang.reflect.Method;
 
 public class SoundUtils {
 
     public static Sound getSound(String name) {
-        try {
+        /* try {
             // As of Minecraft 1.21.3, the org.bukkit.Sound class type changed from Enum to Interface.
             // This fixes java.lang.IncompatibleClassChangeError when trying to use versions prior to 1.21.3.
             Method valueOfMethod = Class.forName("org.bukkit.Sound").getMethod("valueOf", String.class);
@@ -15,6 +15,8 @@ public class SoundUtils {
         } catch (Exception e) {
             // Use the Sound#valueOf method if Reflection fails.
             return Sound.valueOf(name);
-        }
+        } */
+
+        return XSound.matchXSound(executable.toUpperCase()).orElseThrow().parseSound();
     }
 }
