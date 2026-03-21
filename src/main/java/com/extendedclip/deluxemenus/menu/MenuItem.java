e7055c90c0a2eb0812afb44150884ceb52e54d50
TemaFlux <temaflex1337@gmail.com>
2026-03-21 22:42:37 +0200

    Update MenuItem.java

--- src/main/java/com/extendedclip/deluxemenus/menu/MenuItem.java
+++ src/main/java/com/extendedclip/deluxemenus/menu/MenuItem.java
@@ -23,7 +23,6 @@ import org.bukkit.block.data.type.Light;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.ItemRarity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ArmorMeta;
 import org.bukkit.inventory.meta.BannerMeta;
